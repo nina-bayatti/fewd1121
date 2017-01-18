9bae09ff0054790c49470e059798116ba829ca44
nina-bayatti <ninarpm@ninas-air.tpco.loc>
2017-01-18 17:16:32 -0500

    Updated temp converter

---
+++ class11/temp-converter/js/app.js
@@ -0,0 +1,19 @@
+	Fahrenheit = Celsius * 1.8 + 32
+	Celsius = (Fahrenheit - 32) / 1.8
+
+//Set variables for interactive elements
+var units = document.getElementById('units');
+var temp-value = document.getElementById('temp-value');
+
+
+//Set up variables for chunks of data
+var temp-convert;
+
+//Set up trigger
+if (units = Farenheit) {
+	$('#temp-convert').val((temp-value - 32)/1.8);
+}
+
+if (units = Celsius) {
+	$('#temp-convert').val((temp-value * 1.8) + 32);
+}
