04f13db69992f87e3779a516dc2802698e97e02c
nina-bayatti <ninarpm@Ninas-MacBook-Air.local>
2017-02-04 14:26:34 -0500

    Add final project updates

--- homework/Final Project/NinaFinalProject/assets/js/app.js
+++ homework/Final Project/NinaFinalProject/assets/js/app.js
@@ -1,14 +1,15 @@
 console.log('app.js is running!');
 
 $(document).ready (function () {
-	$('.btn-success').click(function () {
+
+	$('.lead').toggleClass('animated bounceInDown');
+	
+	$('.btn-readmore').click(function () {
 	console.log('You clicked the button.')
 	alert('This page is not published yet');
 	});
 
-	$('#rumble-element').hover(function () {
-	$('#rumble-element').trigger('startRumble');
-	console.log('The rumble plugin is working.')
+	$('.btn').hover(function () {
+	$(this).toggleClass('animated jello');
 	});
 });
-
