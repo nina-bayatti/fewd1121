d573b7faffac8119911e92fe3d5bea8dcfe01025
nina-bayatti <ninarpm@Ninas-MacBook-Air.local>
2017-02-05 13:36:40 -0500

    Updated final project

--- homework/Final Project/NinaFinalProject/assets/js/app.js
+++ homework/Final Project/NinaFinalProject/assets/js/app.js
@@ -4,15 +4,20 @@ $(document).ready (function () {
 
 	$('.lead').toggleClass('animated bounceInDown');
 
-	$('.contactForm').click(function () {
-	console.log('A contact was requested.')
-	alert('Unfortunately, our web developer has not learned back-end development yet. Please contact us by sending an email to nbayatti@gmail.com');
+	$('.contactForm').click(function (e) {
+		console.log('A contact was requested.')
+		e.preventDefault();
+		alert('Unfortunately, our web developer has not learned back-end development yet. Please contact us by sending an email to nbayatti@gmail.com');
 	});
 
 	$('.btn').hover(function () {
-	$(this).toggleClass('animated jello');
+		$(this).toggleClass('animated jello');
 	});
 
 	$('.parallax-window').parallax({imageSrc: '../NinaFinalProject/assets/images/stretch-view.jpg'});
+
+	$('#burger').click(function () {
+		$('mobile-nav').show();
+	});
 
 });
