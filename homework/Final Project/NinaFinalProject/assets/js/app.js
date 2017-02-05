0ae0cfee241680aaadf8f67934bf78365b969068
nina-bayatti <ninarpm@Ninas-Air.cable.rcn.com>
2017-02-04 21:51:40 -0500

    Updated final project

--- homework/Final Project/NinaFinalProject/assets/js/app.js
+++ homework/Final Project/NinaFinalProject/assets/js/app.js
@@ -3,13 +3,16 @@ console.log('app.js is running!');
 $(document).ready (function () {
 
 	$('.lead').toggleClass('animated bounceInDown');
-	
-	$('.btn-readmore').click(function () {
-	console.log('You clicked the button.')
-	alert('This page is not published yet');
+
+	$('.contactForm').click(function () {
+	console.log('A contact was requested.')
+	alert('Unfortunately, our web developer has not learned back-end development yet. Please contact us by sending an email to nbayatti@gmail.com');
 	});
 
 	$('.btn').hover(function () {
 	$(this).toggleClass('animated jello');
 	});
+
+	$('.parallax-window').parallax({imageSrc: '../NinaFinalProject/assets/images/stretch-view.jpg'});
+
 });
